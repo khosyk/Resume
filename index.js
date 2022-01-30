19f6b1a165830e042db10e7ec992959a1f6b4545
khosyk <hosy12@gmail.com>
2022-01-30 19:14:06 +0900

    02 about done

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const list = document.querySelectorAll('header ul li');
 const main = document.querySelector('main');
 const section = document.querySelectorAll('main section');
+const button = document.querySelector('#home .text button');
+
+const removeClass= () =>{
+
+  for(let i = 0; i< 4;i++ ){
+    list[i].classList.remove('on');
+    section[i].classList.remove('on');
+    main.classList.remove(`face${i+1}`);
+  }
+}
 
 const handleList = (e, index) =>{
-
+  removeClass();
   // remove all class
-    for(let i = 0; i< 4;i++ ){
-      list[i].classList.remove('on');
-      section[i].classList.remove('on');
-      main.classList.remove(`face${i+1}`);
-    }
   // thene add class to give 3d effect
     main.classList.add(`face${index+1}`);
     e.classList.add('on');
     section[index].classList.add('on');
 }
 
-// const handleFace1 =()=>{
-//   let homeSpan
-//   let homeH1
-//   let homeH2
-// }
-
+const handleClick = () =>{
+  removeClass();
+// thene add class to give 3d effect
+  main.classList.add('face4');
+  list[3].classList.add('on');
+  section[3].classList.add('on');
+}
 
 list.forEach((e, index) => {
   e.addEventListener('click', ()=> handleList(e,index))
 });
+
+button.addEventListener('click', ()=>handleClick());
